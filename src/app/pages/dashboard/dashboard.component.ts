17892c976323f23c161d854c418d7076d5bba09d
TamNguyenMinh <tamnguyenminhse@gmail.com>
2021-03-03 14:59:32 +0700

    login and dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NbMenuItem, NbSidebarService } from '@nebular/theme';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  tools: NbMenuItem[] = [
+    {
+      title: 'Course',
+      icon: 'layers-outline',
+      expanded: true,
+    }
+  ];
+  constructor(
+    private sidebarService: NbSidebarService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  toggle() {
+    this.sidebarService.toggle(true);
+    return false;
+  }
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-
+import { NbActionsModule, NbIconModule, NbLayoutModule, NbMenuModule, NbMenuService, NbSidebarModule, NbSidebarService } from '@nebular/theme';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    DashboardRoutingModule,
+    NbLayoutModule,
+    NbSidebarModule,
+    NbMenuModule,
+    NbIconModule,
+    NbActionsModule
+  ],
+  providers: [NbSidebarService, NbMenuService]
 })
 export class DashboardModule { }
